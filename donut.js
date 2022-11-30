1ef4f98abd216bbd524216c1bd8a1141c6a79097
LarissaDeBorgher <r0839981@student.thomasmore.be>
2022-11-30 19:10:15 +0100

    Feat: add donut to scene

--- donut.js
+++ donut.js
@@ -1,8 +1,12 @@
+
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { Loader } from 'three';
 
 const scene = new THREE.Scene();
-
+// change scene background color
+scene.background = new THREE.Color( 0xE1E0E0 );
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
@@ -12,11 +16,27 @@ document.body.appendChild( renderer.domElement );
 //add orbit controls
 const controls = new OrbitControls(camera, renderer.domElement);
 
-//add ambient light
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-scene.add(ambientLight);
+const dire = new THREE.DirectionalLight(0xffffff, 2);
+dire.position.set(2, 4, 2);
+scene.add(dire);
+camera.position.z = 5;
+camera.position.y = 2;
 
-// add directional light
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-directionalLight.position.set(1, 2, 1);
-scene.add(directionalLight);
+
+
+// add donut 
+let donut;
+const gltfLoader = new GLTFLoader();
+gltfLoader.load('/assets/models/donut-marie.gltf', (gltf) => {
+donut = gltf.scene;
+gltf.scene.scale.set(20,20,20);
+scene.add(donut);
+donut.position.y = 0.5;
+});
+
+function animate() {
+requestAnimationFrame( animate );
+renderer.render( scene, camera );
+}
+
+animate();
