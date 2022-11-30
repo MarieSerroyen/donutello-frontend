b4f9a0db8519c238a7f71edae281b6840062d544
LarissaDeBorgher <r0839981@student.thomasmore.be>
2022-11-30 15:25:11 +0100

    Feat: add orbit controls + light

--- donut.js
+++ donut.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 
@@ -8,3 +9,14 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+//add orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+
+//add ambient light
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+// add directional light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+directionalLight.position.set(1, 2, 1);
+scene.add(directionalLight);
