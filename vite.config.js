686fbb635cfc17eee3f26c86c242c942b92d9a1a
MarieSerroyen <marie.serroyen@gmail.com>
2022-12-13 16:11:55 +0100

    feat: add plugin to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    plugins: ["netlify-plugin-cloudinary"],
+      inputs: {
+        cloudName: "dphelzfrb",
+        deliveryType: "upload",
+        uploadPreset: "ojcpkqqc",
+      },
     build: {
       rollupOptions: {
         input: {
