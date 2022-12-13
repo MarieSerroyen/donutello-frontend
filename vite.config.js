017c5ee181d408fab17eb34eebaa0dbefe0193ee
MarieSerroyen <marie.serroyen@gmail.com>
2022-12-13 15:00:04 +0100

    feat: add confirm.html page

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
       rollupOptions: {
         input: {
           main: 'index.html',
-          configurator: 'configurator.html'
+          configurator: 'configurator.html',
+          confirm: 'confirm.html',
         }
       }
     }
