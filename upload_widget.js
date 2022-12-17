ddc1483e4dc80c46988faf7191c5f2d3dca9cc2e
florianne-denys <r0836751@student.thomasmore.be>
2022-12-17 17:08:51 +0100

    fix: logo upload without widget

--- upload_widget.js
+++ upload_widget.js
@@ -1,45 +1,43 @@
-const cloudName = "dphelzfrb";
-const uploadPreset = "ojcpkqqc";
-let image;
+const cloudName = 'dphelzfrb';
+const unsignedUploadPreset = 'ojcpkqqc';
 
-const myWidget = cloudinary.createUploadWidget(
-  {
-    cloudName: cloudName,
-    uploadPreset: uploadPreset,
-    // cropping: true, //add a cropping step
-    // showAdvancedOptions: true,  //add advanced options (public_id and tag)
-    // sources: [ "local", "url"], // restrict the upload sources to URL and local files
-    // multiple: false,  //restrict upload to a single file
-    // folder: "user_images", //upload files to the specified folder
-    // tags: ["users", "profile"], //add the given tags to the uploaded files
-    // context: {alt: "user_uploaded"}, //add the given context data to the uploaded files
-    // clientAllowedFormats: ["images"], //restrict uploading to image files only
-    // maxImageFileSize: 2000000,  //restrict file size to less than 2MB
-    // maxImageWidth: 2000, //Scales the image down to a width of 2000 pixels before uploading
-    // theme: "purple", //change to a purple theme
-  },
-  (error, result) => {
-    if (!error && result && result.event === "success") {
-      console.log(result.info);
-      image = result.info.secure_url;
-      
-      localStorage.setItem("Logo", image);
+var fileSelect = document.getElementById("fileSelect")
+var fileElem = document.getElementById("fileElem")
+var url;
 
-      document
-        .getElementById("uploadedimage")
-        .setAttribute("src", result.info.secure_url);
-    
-      // add result.info.secure_url to database
-    }else if(error){
-      console.log(error);
-    }
+fileSelect.addEventListener("click", function(e) {
+  if (fileElem) {
+    fileElem.click();
   }
-);
+  e.preventDefault();
+}, false);
 
-document.getElementById("upload_widget").addEventListener(
-  "click",
-  function () {
-    myWidget.open();
-  },
-  false
-);
+function uploadFile(file) {
+  var url = `https://api.cloudinary.com/v1_1/${cloudName}/upload`;
+  var xhr = new XMLHttpRequest();
+  var formData = new FormData();
+  xhr.open('POST', url, true);
+  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+
+
+  xhr.onreadystatechange = function(e) {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      // File uploaded successfully
+      var response = JSON.parse(xhr.responseText);
+      url = response.secure_url;
+      console.log(url);
+      document.querySelector("#uploadedimage").setAttribute("src", url);
+    }
+  };
+
+  formData.append('upload_preset', unsignedUploadPreset);
+  formData.append('tags', 'browser_upload'); // Optional - add tag for image admin in Cloudinary
+  formData.append('file', file);
+  xhr.send(formData);
+}
+
+var handleFiles = function(files) {
+  for (var i = 0; i < files.length; i++) {
+    uploadFile(files[i]); // call the function to upload the file
+  }
+};
