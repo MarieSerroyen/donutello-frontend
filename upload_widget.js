bd9c0affd551d5e119c83a65bfcdaea574786458
florianne-denys <r0836751@student.thomasmore.be>
2022-12-17 21:51:31 +0100

    fix: try different method for logo upload

--- upload_widget.js
+++ upload_widget.js
@@ -1,43 +1,57 @@
-const cloudName = 'dphelzfrb';
-const unsignedUploadPreset = 'ojcpkqqc';
+// const cloudName = 'dphelzfrb';
+// const unsignedUploadPreset = 'ojcpkqqc';
 
-var fileSelect = document.getElementById("fileSelect")
-var fileElem = document.getElementById("fileElem")
-var url;
+// var fileSelect = document.getElementById("fileSelect")
+// var fileElem = document.getElementById("fileElem")
+// var url;
 
-fileSelect.addEventListener("click", function(e) {
-  if (fileElem) {
-    fileElem.click();
-  }
-  e.preventDefault();
-}, false);
+// fileSelect.addEventListener("click", function(e) {
+//   if (fileElem) {
+//     fileElem.click();
+//   }
+//   e.preventDefault();
+// }, false);
 
-function uploadFile(file) {
-  var url = `https://api.cloudinary.com/v1_1/${cloudName}/upload`;
-  var xhr = new XMLHttpRequest();
-  var formData = new FormData();
-  xhr.open('POST', url, true);
-  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+// function uploadFile(file) {
+//   var url = `https://api.cloudinary.com/v1_1/${cloudName}/upload`;
+//   var xhr = new XMLHttpRequest();
+//   var formData = new FormData();
+//   xhr.open('POST', url, true);
+//   xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
 
 
-  xhr.onreadystatechange = function(e) {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      // File uploaded successfully
-      var response = JSON.parse(xhr.responseText);
-      url = response.secure_url;
+//   xhr.onreadystatechange = function(e) {
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//       // File uploaded successfully
+//       var response = JSON.parse(xhr.responseText);
+//       url = response.secure_url;
+//       console.log(url);
+//       document.querySelector("#uploadedimage").setAttribute("src", url);
+//     }
+//   };
+
+//   formData.append('upload_preset', unsignedUploadPreset);
+//   formData.append('tags', 'browser_upload'); // Optional - add tag for image admin in Cloudinary
+//   formData.append('file', file);
+//   xhr.send(formData);
+// }
+
+// var handleFiles = function(files) {
+//   for (var i = 0; i < files.length; i++) {
+//     uploadFile(files[i]); // call the function to upload the file
+//   }
+// };
+
+
+document.querySelector('#inputField').addEventListener('change', function() {
+  console.log(this.files[0]);
+  if (this.files && this.files[0]) {
+      var img = document.querySelector('#uploadedimage');
+      img.onload = () => {
+          URL.revokeObjectURL(img.src);  // no longer needed, free memory
+      }
+      var url = URL.createObjectURL(this.files[0]);
       console.log(url);
-      document.querySelector("#uploadedimage").setAttribute("src", url);
-    }
-  };
-
-  formData.append('upload_preset', unsignedUploadPreset);
-  formData.append('tags', 'browser_upload'); // Optional - add tag for image admin in Cloudinary
-  formData.append('file', file);
-  xhr.send(formData);
-}
-
-var handleFiles = function(files) {
-  for (var i = 0; i < files.length; i++) {
-    uploadFile(files[i]); // call the function to upload the file
+      img.src = url // set src to blob url
   }
-};
+});
