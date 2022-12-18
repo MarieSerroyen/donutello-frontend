07d5687d241565c4b1443e1ecd00a0c71530080c
florianne-denys <r0836751@student.thomasmore.be>
2022-12-18 10:36:20 +0100

    fix: upload widget

--- upload_widget.js
+++ upload_widget.js
@@ -1,3 +1,47 @@
+const cloudName = "dphelzfrb";
+const uploadPreset = "ojcpkqqc";
+let image;
+
+const myWidget = cloudinary.createUploadWidget(
+  {
+    cloudName: cloudName,
+    uploadPreset: uploadPreset,
+    // cropping: true, //add a cropping step
+    // showAdvancedOptions: true,  //add advanced options (public_id and tag)
+    // sources: [ "local", "url"], // restrict the upload sources to URL and local files
+    // multiple: false,  //restrict upload to a single file
+    // folder: "user_images", //upload files to the specified folder
+    // tags: ["users", "profile"], //add the given tags to the uploaded files
+    // context: {alt: "user_uploaded"}, //add the given context data to the uploaded files
+    // clientAllowedFormats: ["images"], //restrict uploading to image files only
+    // maxImageFileSize: 2000000,  //restrict file size to less than 2MB
+    // maxImageWidth: 2000, //Scales the image down to a width of 2000 pixels before uploading
+    // theme: "purple", //change to a purple theme
+  },
+  (error, result) => {
+    if (!error && result && result.event === "success") {
+      console.log(result.info);
+      image = result.info.secure_url;
+      document
+        .getElementById("uploadedimage")
+        .setAttribute("src", result.info.secure_url);
+    
+      // add result.info.secure_url to database
+    }else if(error){
+      console.log(error);
+    }
+  }
+);
+
+document.getElementById("upload_widget").addEventListener(
+  "click",
+  function () {
+    myWidget.open();
+  },
+  false
+);
+
+
 // const cloudName = 'dphelzfrb';
 // const unsignedUploadPreset = 'ojcpkqqc';
 
@@ -56,23 +100,27 @@
 //   }
 // });
 
-let logoUrl;
 
-const createUrl = () => {
-  let logo = document.querySelector("#fileElem").files[0];
-  let formData = new FormData();
-  formData.append("file", logo);
-  formData.append("upload_preset", "ojcpkqqc");
-  fetch("https://api.cloudinary.com/v1_1/dphelzfrb/image/upload", {
-      method: "POST",
-      body: formData
-  })
-  .then(response => response.json())
-  .then(data => {
-      logoUrl = data.secure_url;
-      // createDonut();
-      console.log(logoUrl);
-  })
-}
 
-document.querySelector("#fileElem").addEventListener("change", createUrl);
+
+
+// let logoUrl;
+
+// const createUrl = () => {
+//   let logo = document.querySelector("#fileElem").files[0];
+//   let formData = new FormData();
+//   formData.append("file", logo);
+//   formData.append("upload_preset", "ojcpkqqc");
+//   fetch("https://api.cloudinary.com/v1_1/dphelzfrb/image/upload", {
+//       method: "POST",
+//       body: formData
+//   })
+//   .then(response => response.json())
+//   .then(data => {
+//       logoUrl = data.secure_url;
+//       // createDonut();
+//       console.log(logoUrl);
+//   })
+// }
+
+// document.querySelector("#fileElem").addEventListener("change", createUrl);
